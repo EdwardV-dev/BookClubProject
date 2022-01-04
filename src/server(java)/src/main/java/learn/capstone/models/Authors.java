ce6393d2e309ef877ad4286b609afe42d08eb034
Edward V <edwardveytsman13@gmail.com>
2022-01-04 10:00:55 -0800

    Added some models

---
+++ src/server(java)/src/main/java/learn/capstone/models/Authors.java
@@ -0,0 +1,45 @@
+package learn.capstone.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Authors {
+    private int idAuthor;
+
+    private String authorFirstName;
+    private String authorLastName;
+
+    private List<Books> books = new ArrayList<>();
+
+    public int getIdAuthor() {
+        return idAuthor;
+    }
+
+    public void setIdAuthor(int idAuthor) {
+        this.idAuthor = idAuthor;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
+
+    public List<Books> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Books> books) {
+        this.books = books;
+    }
+}
