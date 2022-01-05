00ee9c9d83e0629c2743a1fba80f49ce94bcad34
Edward V <edwardveytsman13@gmail.com>
2022-01-05 09:18:46 -0800

    gitkeep added for react

--- src/server(java)/src/main/java/learn/capstone/models/AppUserBooks.java
+++ src/server(java)/src/main/java/learn/capstone/models/AppUserBooks.java
@@ -4,4 +4,30 @@ public class AppUserBooks {
     private int appUserId;
     private String completionStatus;
 
+    private Books book;
+
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getCompletionStatus() {
+        return completionStatus;
+    }
+
+    public void setCompletionStatus(String completionStatus) {
+        this.completionStatus = completionStatus;
+    }
+
+    public Books getBook() {
+        return book;
+    }
+
+    public void setBook(Books book) {
+        this.book = book;
+    }
 }
