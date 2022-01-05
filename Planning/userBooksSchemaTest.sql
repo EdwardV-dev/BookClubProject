d2a19898bfa64035466b35ae199e9e24f975433b
Edward V <edwardveytsman13@gmail.com>
2022-01-05 07:54:19 -0800

    Sql Test Updated

--- Planning/userBooksSchemaTest.sql
+++ Planning/userBooksSchemaTest.sql
@@ -162,6 +162,8 @@ delimiter ;
         ( "Christopher", "Robin"),
         ("JK", "Rowling"),
         ("Henry", "Smith");
+        
+     -- testing here
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
