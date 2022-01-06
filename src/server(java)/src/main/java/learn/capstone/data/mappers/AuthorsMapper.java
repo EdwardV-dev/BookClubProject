fd41c007e4b572e5cf3bb9e0cda287131b475297
Edward V <edwardveytsman13@gmail.com>
2022-01-06 08:25:27 -0800

    Updated authors mapper and authors model

--- src/server(java)/src/main/java/learn/capstone/data/mappers/AuthorsMapper.java
+++ src/server(java)/src/main/java/learn/capstone/data/mappers/AuthorsMapper.java
@@ -1,12 +1,23 @@
 package learn.capstone.data.mappers;
 
+import learn.capstone.models.Authors;
 import learn.capstone.models.Books;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AuthorsMapper {
+public class AuthorsMapper implements RowMapper<Authors> {
 
 
+    @Override
+    public Authors mapRow(ResultSet resultSet, int i) throws SQLException {
+        Authors author = new Authors();
+
+        author.setIdAuthor(resultSet.getInt("idAuthor"));
+        author.setAuthorFirstName(resultSet.getString("author_first_name"));
+        author.setAuthorLastName(resultSet.getString("author_last_name"));
+        
+        return author;
+    }
 }
