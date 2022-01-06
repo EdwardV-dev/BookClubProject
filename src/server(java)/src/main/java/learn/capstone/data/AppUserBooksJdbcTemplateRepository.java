b1935c14224a7f318c98e2a3564aeef3c00ff321
David Gomez <david.a.gomez10@gmail.com>
2022-01-06 11:04:43 -0800

    Completed update completion status for user books.

--- src/server(java)/src/main/java/learn/capstone/data/AppUserBooksJdbcTemplateRepository.java
+++ src/server(java)/src/main/java/learn/capstone/data/AppUserBooksJdbcTemplateRepository.java
@@ -1,4 +1,27 @@
 package learn.capstone.data;
 
+import learn.capstone.models.AppUserBooks;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AppUserBooksJdbcTemplateRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public AppUserBooksJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean update(AppUserBooks appUserBooks) {
+        final String sql = "update app_user_has_books set "
+                + "completion_status = ? "
+                + "where idBooks = ? and app_user_id = ?;";
+
+        return jdbcTemplate.update(sql,
+                appUserBooks.getCompletionStatus(),
+                appUserBooks.getBook().getIdBooks(),
+                appUserBooks.getAppUserId()) > 0;
+    }
 }
---
+++ src/server(java)/src/test/java/learn/capstone/data/AppUserBooksJdbcTemplateRepositoryTest.java
@@ -0,0 +1,36 @@
+package learn.capstone.data;
+
+import learn.capstone.models.AppUserBooks;
+import learn.capstone.models.Books;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AppUserBooksJdbcTemplateRepositoryTest {
+
+    @Autowired
+    AppUserBooksJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() { knownGoodState.set(); }
+
+    @Test
+    void shouldUpdateCompletionStatus() {
+        Books book = new Books();
+        book.setIdBooks(2);
+
+        AppUserBooks userBook = new AppUserBooks();
+        userBook.setBook(book);
+        userBook.setAppUserId(1);
+        userBook.setCompletionStatus("DoneReading");
+
+        assertTrue(repository.update(userBook));
+    }
+}
