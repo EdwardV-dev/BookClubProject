c07dcb497e54b4ea9553469780881a63b517cc21
David Gomez <david.a.gomez10@gmail.com>
2022-01-06 08:13:30 -0800

    Added Search Bar in book list and separated authors first and last name..

--- src/client(react)/bookclub-frontend/src/components/MyBooks.js
+++ src/client(react)/bookclub-frontend/src/components/MyBooks.js
@@ -1,10 +1,21 @@
-
+import { Link, useHistory } from "react-router-dom";
 
 function MyBooks() {
 
+    const history = useHistory();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        //history.push("/books");
+    }
 
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input type="text" id="search" name="search" placeholder="Author Last/First Name" />
+                <button type="submit">Search</button>
+            </form>
             <div>
                 <button type="button" className="btn btn-primary mx-2 my-4">
                     Add Book
@@ -15,14 +26,12 @@ function MyBooks() {
                 <thead>
                     <tr>
                         <th scope="col">Book Title</th>
-                        <th scope="col">Book Author</th>
                         <th scope="col">Genre</th>
                         <th scope="col">Year Published</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>test1</td>
                         <td>test1</td>
                         <td>test1</td>
                         <td>test1</td>
