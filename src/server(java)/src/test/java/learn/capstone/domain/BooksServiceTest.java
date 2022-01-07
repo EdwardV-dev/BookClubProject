7fa681eb62fae5ef6f8e2b8ce6e1e10393f5373b
David Gomez <david.a.gomez10@gmail.com>
2022-01-07 08:37:47 -0800

    Completed update in BooksService with tests.

--- src/server(java)/src/test/java/learn/capstone/domain/BooksServiceTest.java
+++ src/server(java)/src/test/java/learn/capstone/domain/BooksServiceTest.java
@@ -1,6 +1,7 @@
 package learn.capstone.domain;
 
 import learn.capstone.data.BooksRepository;
+import learn.capstone.models.Authors;
 import learn.capstone.models.Books;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class BooksServiceTest {
 
     @Autowired
@@ -21,15 +22,43 @@ class BooksServiceTest {
 
     @Test
     void shouldUpdate() {
+        Books book = makeBook();
+
+        when(repository.update(book)).thenReturn(true);
+        Result<Books> actual = service.updateAdmin(book);
+        assertEquals(ResultType.SUCCESS, actual.getType());
+    }
+
+    @Test
+    void shouldNotUpdateMissing() {
+        Books book = makeBook();
+
+        when(repository.update(book)).thenReturn(false);
+        Result<Books> actual = service.updateAdmin(book);
+        assertEquals(ResultType.NOT_FOUND, actual.getType());
+    }
+
+    @Test
+    void shouldNotUpdateInvalid() {
+        Books book = makeBook();
+        book.setYearPublished(2030);
+
+        when(repository.update(book)).thenReturn(true);
+        Result<Books> actual = service.updateAdmin(book);
+        assertEquals(ResultType.INVALID, actual.getType());
+    }
+
+    Books makeBook() {
+        Authors author = new Authors();
+        author.setIdAuthor(1);
+
         Books book = new Books();
         book.setIdBooks(2);
         book.setBookTitle("Test");
         book.setApprovalStatus(true);
-        book.setYearPublished(1996);
+        book.setYearPublished(1999);
         book.setGenre("fiction");
-
-        when(repository.update(book)).thenReturn(true);
-        Result<Books> actual = service.update(book);
-        assertEquals(ResultType.SUCCESS, actual.getType());
+        book.setAuthor(author);
+        return book;
     }
 }
