4a22a4a3c99a78ec8be0b8e9e9f4de746ff94c95
Edward V <edwardveytsman13@gmail.com>
2022-01-10 09:16:10 -0800

    Error component implemented

--- src/client(react)/bookclub-frontend/src/components/Error.js
+++ src/client(react)/bookclub-frontend/src/components/Error.js
@@ -1,3 +1,15 @@
-export default function Error (){
-  
+import { useHistory } from "react-router-dom";
+
+function Error({ msg }) {
+  const history = useHistory();
+
+  return (
+    <p>
+      🙅🏾‍♂️ Error{" "}
+      {history.location.state ? ` - ${history.location.state.msg}` : ""}
+      {msg}
+    </p>
+  );
 }
+
+export default Error;
