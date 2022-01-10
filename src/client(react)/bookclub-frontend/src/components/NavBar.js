2ac8f1af95a2f5378a71eb61dac20181280c7d22
David Gomez <david.a.gomez10@gmail.com>
2022-01-10 11:10:23 -0800

    pull from git.

--- src/client(react)/bookclub-frontend/src/components/NavBar.js
+++ src/client(react)/bookclub-frontend/src/components/NavBar.js
@@ -1,39 +1,11 @@
-import { Link, useHistory } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
-import { useContext, useEffect } from "react";
-import jwtDecode from "jwt-decode";
 
-function NavBar({role}) {
-    const [userStatus, setUserStatus] = useContext(AuthContext); 
 
-      // "user" represents an object with multiple properties
-      useEffect(() => {
-        const token = localStorage.getItem("token");
-        if (token) {
-          setUserStatus({ user: jwtDecode(token) });
-        }
-      }, [setUserStatus]);
-
+function NavBar() {
     return (
         <nav>
-            {userStatus?.user ? (
-           <li>
-            <button
-              onClick={() => {
-                setUserStatus(null);
-                localStorage.removeItem("token");
-          
-              }}
-            >
-              {/* `sub` is the property from the decoded token */}
-               Logout {userStatus.user.sub}
-             </button>
-           </li>
-        ) : (
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-       )}
+            <button type="button" className="btn btn-success ml-2">
+                Login
+            </button>
             <button type="bugit sttustton" className="btn btn-success ml-2">
                 Register
             </button>
