c187b8c063622b1a5f6014b5a3e6d45bf030dedf
Edward V <edwardveytsman13@gmail.com>
2022-01-12 20:35:19 -0800

    Fixed completion status bug in add books component. Added some input validation to addbook

--- Planning/sampleQueriesForTesting.sql
+++ Planning/sampleQueriesForTesting.sql
@@ -13,7 +13,10 @@ Select *
 from books b
 Inner join authors au on b.idAuthor = au.idAuthor;
 
-Select author_first_name, author_last_name
+select *
+from app_user_has_books;
+
+Select *
 from authors;
 
 Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor
@@ -28,7 +31,7 @@ Select r.role_name, au.username
                 where au.username = "Mozart5";
                 
                 
-Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, ab.completion_status, au.author_last_name
+Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, ab.completion_status, au.author_last_name, au.author_first_name
 from books b
 inner join app_user_has_books ab on ab.idBooks = b.idBooks
 Inner join authors au on b.idAuthor = au.idAuthor
@@ -43,13 +46,22 @@ GROUP BY genre
 Order by genreCount desc
 limit 1; -- fiction should be the result of this query. The result of this query is plugged into the query below via the "where clause"
 
-Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, au.author_first_name, au.author_last_name
+Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, au.author_first_name, au.author_last_name,
+(Select Count(*) from books b where b.genre= "fiction" and b.approval_status = true) -- only one fiction book is approved
 from books b
 Inner join authors au 
 on b.idAuthor = au.idAuthor
 where b.genre= "fiction"
-Order by RAND()
-Limit 1;
+Order by RAND();
+
+
+Select * From
+(Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, au.author_first_name, au.author_last_name,
+ROW_NUMBER() OVER(Order by idBooks) as row_numbering
+from books b
+Inner join authors au 
+on b.idAuthor = au.idAuthor) as innerTable
+where genre= "fiction" and  row_numbering = 2;
 
 
 Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, au.author_first_name 
@@ -57,5 +69,18 @@ Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, 
                 Inner join authors au 
                 on b.idAuthor = au.idAuthor
                 where au.author_first_name = "Henry" and au.author_last_name = "Smith" and b.book_title = "Fossils and more!";
-                
-                
+ 
+ SET SQL_SAFE_UPDATES=0;
+ update books set publication_year = 6000 where publication_year is null;
+ SET SQL_SAFE_UPDATES=1;     
+ 
+ insert into app_user_has_books (app_user_id, completion_status, idBooks)
+ values
+ (1, "WantToRead", 18);
+ 
+ Select b.book_title, b.genre, b.idBooks, b.approval_status, b.publication_year, b.idAuthor, ab.completion_status, ab.app_user_id,
+                 au.author_first_name, au.author_last_name from books b
+                Inner join authors au 
+                on b.idAuthor = au.idAuthor
+                Inner join app_user_has_books ab on ab.idBooks = b.idBooks
+                where (author_first_name = "JK" or author_last_name = "Rowling") and ab.app_user_id = 1;
